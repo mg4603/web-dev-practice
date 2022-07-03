521c6bc957555da11593c58dd15a78d5edea8d56
Michael George <michael.g.george@gmail.com>
2022-07-03 13:51:53 +0530

    Complete tasks for variables lesson in js-fundamentals-1

---
+++ js/fundamentals-1.js
@@ -0,0 +1,14 @@
+// Tasks for variables
+// 1) Working with variables
+let name, admin;
+name = "John";
+admin = name;
+alert(admin);
+
+// 2) Giving the right name
+let ourPlanetName="Earth";
+let currentUserName;
+
+// 3) Uppercase const?
+const BIRTHDAY = "18.04.1982";
+const age = "" // some function on age;
